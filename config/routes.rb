03b0ff2d54766a374238540f3bd1f9c5cf1226cb
migyur <migyur0928@gmail.com>
2020-09-23 11:51:31 -0700

    Revert "create post MVC"
    This reverts commit a3997d5c2509a87213a420eb2e4d52e5851303c8.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+
   get "user/log_in" => "user#login"
   get "user/sign_up" => "user#signup"
   root "user#login"
 
-  resources :posts
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
