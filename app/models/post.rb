ae8f866144b26d15d820b24734782194b5e1d770
wjmtutu <71449315+wjmtutu@users.noreply.github.com>
2020-11-04 23:39:27 -0500

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ class Post < ApplicationRecord
             length: {maximum: 100}
   validates :text, presence: { message: "text cannot be empty" },
             length: {maximum: 200}
+  attr_accessor :author
+  attr_accessor :vote
   #attr_accessor :anonymous
   #validates :vote
 end
