e6d247cdf6213bb2037af67102d382dbd8d69b4c
wjmtutu <71449315+wjmtutu@users.noreply.github.com>
2020-11-04 23:37:51 -0500

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+    @comment.commenter = current_user.username
     if @comment.save
       redirect_to post_path(@post)
       end
@@ -16,6 +17,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:commenter, :text)
+    params.require(:comment).permit(:text)
   end
 end
